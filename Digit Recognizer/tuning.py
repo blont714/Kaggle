34cf1ea3a4255031c3045546196d4186a84318ad
blont714 <33482172+blont714@users.noreply.github.com>
2019-07-22 17:03:45 +0900

    Update tuning.py

--- Digit Recognizer/tuning.py
+++ Digit Recognizer/tuning.py
@@ -40,16 +40,6 @@ def prepare_data():
                                                         test_size=0.1,
                                                         random_state=random_seed)
 
-    """
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train = x_train.reshape(60000, 784)
-    x_test = x_test.reshape(10000, 784)
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-    y_train = to_categorical(y_train, 10)
-    y_test = to_categorical(y_test, 10)
-    """
-    print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
     return x_train, y_train, x_test, y_test
 
 def create_model(x_train, y_train, x_test, y_test):
